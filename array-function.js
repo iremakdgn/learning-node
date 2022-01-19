8599ad7701ea2626ab9212640c719cf1be540f3a
iremakdgn <irem.akdgn.1996@gmail.com>
2022-01-19 20:08:48 +0300

    array functions

---
+++ array-function.js
@@ -0,0 +1,21 @@
+/*
+push
+map
+find
+filter
+some
+every
+includes
+ */
+
+//push
+const users = ["Mehmet", "Ahmet", "Ali"]
+
+users.push('Ayşe')
+
+console.log(users)
+
+//map
+users.map((item) => {
+    console.log(item);
+})
